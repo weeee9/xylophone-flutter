644d10a6f0f137b82a5e38748608fedce14d0ddb
周身鴻 <weeee9@MacBookAir.local>
2022-11-16 21:14:48 +0800

    feat: xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,52 @@ void main() {
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSouncd(int soundNumber) {
+    final player = AudioPlayer();
+
+    DartPluginRegistrant.ensureInitialized();
+
+    player.play(
+      AssetSource('note$soundNumber.wav'),
+    );
+  }
+
+  Expanded buildXylophoneKey(
+      {required note, required int sound, required Color color}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () => playSouncd(sound),
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        child: Text(
+          note,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                DartPluginRegistrant.ensureInitialized();
-                player.play(
-                  AssetSource('note1.wav'),
-                );
-              },
-              child: const Text("click me"),
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildXylophoneKey(note: "C", sound: 1, color: Colors.red),
+              buildXylophoneKey(note: "D", sound: 2, color: Colors.orange),
+              buildXylophoneKey(note: "E", sound: 3, color: Colors.yellow),
+              buildXylophoneKey(note: "F", sound: 4, color: Colors.green),
+              buildXylophoneKey(note: "G", sound: 5, color: Colors.teal),
+              buildXylophoneKey(note: "A", sound: 6, color: Colors.blue),
+              buildXylophoneKey(note: "B", sound: 7, color: Colors.purple)
+            ],
           ),
         ),
       ),
